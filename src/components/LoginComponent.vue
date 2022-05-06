ddc99a092ecf9c3f4b25b6cf125e7861578ed860
guillemmiro <guillem.miro@students.salle.url.edu>
2022-05-06 18:01:59 +0200

    Added the necessary files from the zip and corrected errors. It is working.

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,235 @@
+<template>
+  <main>
+    <section>
+      <div class="flex-container">
+        <div class="flex-items">
+          <img class="party-photo" alt="Profile picture" src="../assets/party_image.jpeg">
+        </div>
+        <div class="flex-items">
+          <h1>OpenEvents</h1>
+        </div>
+      </div>
+    </section>
+    <section>
+
+      <form class="flex-form-container">
+        <input v-model="email" type="text" placeholder="Email" name="email" required>
+        <input v-model="password" type="password" placeholder="Password" name="password" required>
+        <button v-on:click="login(email,password)" type="submit">Login</button>
+      </form>
+
+      <div class="flex-form-register">
+        <div class="flex-items-register"><a>Not registered?</a></div>
+        <div class="flex-items-register"><a href="">Register</a></div>
+      </div>
+    </section>
+
+  </main>
+  <footer>
+    <div class="flex-container-icons">
+      <div><a href="#" class="fa fa-facebook" style="color: white"></a></div>
+      <div><a href="#" class="fa fa-twitter" style="color: white"></a></div>
+      <div><a href="#" class="fa fa-google" style="color: white"></a></div>
+    </div>
+  </footer>
+
+</template>
+
+<script>
+export default {
+  name: "LoginComponent",
+
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    login(email, password) {
+      alert(email + ' ' + password);
+      fetch('http://puigmal.salle.url.edu/api/v2/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      }).then(response => response.json()).then(data => alert(data.data));
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+body {
+  background-color: #EFEFEF;
+}
+
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  align-content: stretch;
+  margin-top: 5%;
+}
+
+.flex-items:nth-child(1) {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+  margin-right: -10%;
+}
+
+.flex-items:nth-child(2) {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+}
+
+.party-photo {
+  width: 60%;
+}
+
+h1 {
+  font-family: "Brush Script MT", serif;
+  font-size: 100px;
+}
+
+hr {
+  width: 60%;
+}
+
+.flex-form-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  align-content: stretch;
+}
+
+input[type=text], input[type=password] {
+  width: 30%;
+  padding: 12px 200px 12px 40px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: lightgray;
+  color: white;
+  padding: 14px 20px;
+  border: none;
+  cursor: pointer;
+  border-radius: 8%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.flex-form-register {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  align-content: stretch;
+  margin-top: 1%;
+}
+
+.flex-items-register:nth-child(2) {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+  margin-left: 1%;
+}
+
+.flex-items h1 {
+  font-size: 5.5em;
+}
+
+#title-register {
+  font-size: 5.5em;
+  text-align: center;
+}
+
+
+a {
+  font-family: Verdana, serif;
+}
+
+a:visited {
+  text-decoration: none;
+  color: navy;
+}
+
+.flex-container-icons {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: right;
+  align-items: center;
+  align-content: stretch;
+  margin-top: 5%;
+  margin-right: 5%;
+}
+
+footer {
+  position: fixed;
+  padding: 10px 10px 0 10px;
+  bottom: 0;
+  width: 100%;
+}
+
+.fa {
+  padding: 10px;
+  font-size: 30px;
+  text-decoration: none;
+  border-radius: 50%;
+  margin-left: 1px;
+}
+
+.fa-facebook {
+  background: #3B5998;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+}
+
+.fa-google {
+  background: #dd4b39;
+}
+
+.flex-container-icons {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: right;
+  align-items: flex-end;
+  align-content: stretch;
+  margin-top: 5%;
+  margin-right: 2%;
+}
+
+</style>
